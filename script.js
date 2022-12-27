e53ad972e2355d502297bd6bd9e5b6664e8bb066
Matthias Molina <maty161616@gmail.com>
2022-12-26 21:31:25 -0300

    updates

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
+// !IMPORTANT eliminar todos los prompt, alert y clg
 
-class destino {
-    constructor(continente, pais) {
-        this.continente = continente;
-        this.pais = pais;
-    }
+function destino(continente, pais1, pais2, pais3) {
+    this.continente = continente;
+    this.pais1 = pais1;
+    this.pais2 = pais2;
+    this.pais1 = pais3;
 }
 
-/* mi idea, mas adelante, es agregar 5 botones que desplieguen los paises disponibles por cada continente */
+
  
-const asia = new destino("Asia", "China");
-const america = new destino("America", "Argentina");
-const africa = new destino("Africa", "Uganda");
-const europa = new destino("Europa", "Italia");
-const oceania = new destino("Oceania", "Australia");
+const america = new destino("America", "Argentina", "México", "Estados Unidos");
+const europa = new destino("Europa", "Alemania", "Italia", "España");
+const asia = new destino("Asia", "China", "India", "Indonesia");
+const africa = new destino("Africa", "Uganda", "Marruecos", "Egipto");
+const oceania = new destino("Oceania", "Australia", "Nueva Zelanda", "Fiji");
 
 const destinoDisponible = []
 destinoDisponible.push(asia)
 destinoDisponible.push(america)
 destinoDisponible.push(africa)
 destinoDisponible.push(europa)
-destinoDisponible.push(oceania)
+destinoDisponible.push(oceania)  
 
-console.log(destinoDisponible);
+console.log(destinoDisponible); // eliminar este clg
 
 function filtrarDestinosPorContinente(destinos, continente) {
     return destinos.filter(d => d.continente === continente);
 }
 
 const destinosEnAsia = filtrarDestinosPorContinente(destinoDisponible, 'Asia');
-console.log(destinosEnAsia);
+console.log(destinosEnAsia); //eliminar este clg
 
 fondosTotales = parseInt(prompt("¿Cuanto dinero posee?"))
 
@@ -38,7 +39,7 @@ function presupuesto() {
     transporte = parseInt(prompt("ingrese costo de transporte"));
     suma = vuelo + hospedaje + transporte;
 
-    alert("El gasto total del viaje es " + suma);
+    alert("El gasto total del viaje es " + suma); // mostrar mensaje debajo de los primeros inputs
 }
 presupuesto()
 
@@ -55,7 +56,7 @@ let result = fondosTotales - resultadoSuma
 
 alert(`Su dinero restante es de: ${r}`);
 
-let semanas = 0;
+let semanas = 0; // corregir este loop
 do {
     alert(`Faltan ${semanas} semanas para su viaje`);
     semanas++;
