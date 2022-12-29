979eedbe5c8cacbb7d0825b62e7b9d31d8e7a2b4
Matthias Molina <maty161616@gmail.com>
2022-12-29 18:31:08 -0300

    commit entrega 3

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 
+function destino(continente, pais1, pais2, pais3) {
+  this.continente = continente;
+  this.pais1 = pais1;
+  this.pais2 = pais2;
+  this.pais3 = pais3;
+}
+
+const america = new destino("America", "Argentina", "México", "Estados Unidos");
+const europa = new destino("Europa", "Alemania", "Italia", "España");
+const asia = new destino("Asia", "China", "India", "Indonesia");
+const africa = new destino("Africa", "Uganda", "Marruecos", "Egipto");
+const oceania = new destino("Oceania", "Australia", "Nueva Zelanda", "Fiji");
+
+
+const destinoDisponible = []
+destinoDisponible.push(asia)
+destinoDisponible.push(america)
+destinoDisponible.push(africa)
+destinoDisponible.push(europa)
+destinoDisponible.push(oceania)
+
+const aJson = JSON.stringify(destinoDisponible)
+sessionStorage.setItem("Destinos disponible", aJson)
+
 
 let fondosTotales = document.getElementById("fondosTotales")
 fondosTotales.addEventListener("submit", presupuesto)
 
 function presupuesto(e) {
-
-    e.preventDefault();
 
     let budget = document.getElementById("budget").value
     let vuelo = document.getElementById("vuelo").value
