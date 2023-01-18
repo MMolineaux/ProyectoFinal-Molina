64825b653c3502f08abd67acfa0a86b03ef1564f
Matthias Molina <maty161616@gmail.com>
2023-01-18 10:33:23 -0300

    json + cambios

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
+fetch('./data.json')
+  .then(response => response.json())
+  .then(data => {
+    function buscarData(searchTerm, continent) {
+      let resultados = [];
+      if (continent === 'all') {
+        for (let c in data) {
+          for (let i = 0; i < data[c].countries.length; i++) {
+            if (data[c].countries[i].name.toLowerCase().includes(searchTerm.toLowerCase())) {
+              resultados.push(data[c].countries[i]);
+            }
+          }
+        }
+      } else {
+        for (let i = 0; i < data[continent].countries.length; i++) {
+          if (data[continent].countries[i].name.toLowerCase().includes(searchTerm.toLowerCase())) {
+            resultados.push(data[continent].countries[i]);
+          }
+        }
+      }
+      return resultados;
+    }
 
-function destino(continente, pais1, pais2, pais3) {
+
+    let searchInput = document.querySelector('#search-input');
+    let searchButton = document.querySelector('#search-button');
+    let continentSelect = document.querySelector('#continent-select');
+    searchButton.addEventListener('click', function () {
+      let searchResults = buscarData(searchInput.value, continentSelect.value);
+
+      displayResults(searchResults);
+    });
+  });
+
+
+function displayResults(results) {
+  let resultsContainer = document.querySelector('#results-container');
+  resultsContainer.innerHTML = '';
+  for (let i = 0; i < results.length; i++) {
+    let item = document.createElement('p');
+    item.innerHTML = `País: ${results[i].name} <br> Capital: ${results[i].capital}`;
+    resultsContainer.appendChild(item);
+  }
+
+
+}
+
+/* function destino(continente, pais1, pais2, pais3) {
   this.continente = continente;
   this.pais1 = pais1;
   this.pais2 = pais2;
@@ -18,16 +64,18 @@ destinoDisponible.push(asia)
 destinoDisponible.push(america)
 destinoDisponible.push(africa)
 destinoDisponible.push(europa)
-destinoDisponible.push(oceania)
+destinoDisponible.push(oceania) */
 
-const aJson = JSON.stringify(destinoDisponible)
-sessionStorage.setItem("Destinos disponible", aJson)
+/* const aJson = JSON.stringify(destinoDisponible)
+sessionStorage.setItem("Destinos disponible", aJson) */
 
 
 let fondosTotales = document.getElementById("fondosTotales")
 fondosTotales.addEventListener("submit", presupuesto)
 
 function presupuesto(e) {
+
+  e.preventDefault()
 
     let budget = document.getElementById("budget").value
     let vuelo = document.getElementById("vuelo").value
